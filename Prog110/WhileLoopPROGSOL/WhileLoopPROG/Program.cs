186106de9f9450d25723843673313314fd28f490
Yusuf <yhanafy18@gmail.com>
2020-01-26 14:14:33 -0800

    While Loop Program
    This Program asks the user to enter a word over and over. When the user is done, my program will write out the longest word of all the words they entered.

---
+++ Prog110/WhileLoopPROGSOL/WhileLoopPROG/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WhileLoopPROG
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ask the user to enter the first word, 
+            Console.WriteLine("Please enter a word: ");
+            string word = Console.ReadLine();
+            string currentWinner = "";
+
+            while (word != "end")
+            {
+                int numLetter = word.Length;
+                int finalnum = currentWinner.Length;
+                if (numLetter > finalnum)
+                {
+                    currentWinner = word;
+                }
+                Console.WriteLine("Please enter a word:        Type 'end' to quit. ");
+                word = Console.ReadLine();
+            }
+               
+            Console.WriteLine(currentWinner);
+            Console.ReadLine();
+        }
+    }
+}
